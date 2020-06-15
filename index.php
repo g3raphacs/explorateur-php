e0024bca041f49cbdb28e0bc263d851a10036a56
Jeremy Mathieu <g3raphacs@gmail.com>
2020-06-15 11:34:33 +0200

    init

--- index.php
+++ index.php
@@ -1,26 +1,24 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Explorer</title>
+    <!-- lien Bootstrap CDN  -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <h1>TEST TEST TEST</h1>
 
-    $url = getcwd(); 
-    $content=scandir($url);
 
-    print_r($content);
-  
-    foreach($content as $item ){
-        // echo $item."<br />";
-    }
 
-    //initialisation
-    $home="home";
-    if( !is_dir($home)){
-        mkdir('home');
-    }
-    chdir(getcwd() . DIRECTORY_SEPARATOR . $home);
-    $url = getcwd(); 
 
-    $content=scandir($url);
 
-    // print_r($content);
 
-    foreach($content as $item) {
-        echo $item."<br />";
-    }
+
+    <!-- scrips necessaires à bootstrap  -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
